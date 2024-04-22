42ba720ef564b6e0e7266a9793485f4946dac06e
MSI\takahara <jihyun8286@gmail.com>
2024-04-22 11:28:15 +0900

    水面を追加　底面のディスプレイの向きを変更　色を追加

--- Assets/Scrips/UpDownObject.cs
+++ Assets/Scrips/UpDownObject.cs
@@ -3,24 +3,46 @@ using System.Collections;
 
 public class UpDownObject : MonoBehaviour
 {
+    [SerializeField] GameObject sphere;
+    [SerializeField] GameObject cylinder;
+    private struct PhysicsState
+    {
+        public float pos_x;
+        public float pos_y;
+        public float pos_z;
+        public float vel_x;
+        public float vel_y;
+        public float vel_z;
+    }
+    private PhysicsState sphereState;
+    private PhysicsState cylinderState;
 
     private Vector3 initialPosition;
-    private const float G=9.8f * 0.25f;
-    private const float GROUND_Y = 0.018f;
+    private const float G=9.8f * 0.25f; // 落下速度調整
+    private const float GROUND_Y = 0.0f; // 水滴の落ちる位置
     private float gravity = G;
-    private float x;
-    private float y;
-    private float z;
-    private float y_vel=0;
+    private float radius_coef = 1.0f; //radius = -sphereState.pos_y * radius_coef;
     private int SpacePushedCount=0;
     private bool Manual = false;
+    private bool sphereIsActive = true; 
 
     void Start()
     {
-        initialPosition = transform.position;
-        x = initialPosition.x;
-        y = initialPosition.y;
-        z = initialPosition.z;
+        initialPosition = sphere.transform.position;
+        sphereState.pos_x = initialPosition.x;
+        sphereState.pos_y = initialPosition.y;
+        sphereState.pos_z = initialPosition.z;
+        sphereState.vel_x = 0.0f;
+        sphereState.vel_y = 0.0f;
+        sphereState.vel_z = 0.0f;
+
+        // cylinderはsphereの真下にくる
+        cylinderState.pos_x = initialPosition.x;
+        cylinderState.pos_y = GROUND_Y;
+        cylinderState.pos_z = initialPosition.z;
+        cylinderState.vel_x = 0.0f;
+        cylinderState.vel_y = 0.0f;
+        cylinderState.vel_z = 0.0f;
     }
 
     void Update()
@@ -34,28 +56,44 @@ public class UpDownObject : MonoBehaviour
             Manual = true;
         }
         
-        if(Manual){
+        if(Manual){ // 水滴の高さを手動で調整
             if(Input.GetKeyDown(KeyCode.UpArrow)){
-                y += 0.001f;
+                sphereState.pos_y += 0.001f;
             }else if(Input.GetKeyDown(KeyCode.DownArrow)){
-                y -= 0.001f;
+                sphereState.pos_y -= 0.001f;
             }   
         }else{
-            if(y > GROUND_Y){
+            if(sphereState.pos_y > GROUND_Y){
                 gravity = G;
             }else{
                 gravity = -G;
             }
-            y_vel += gravity*Time.deltaTime;
-            y -= y_vel*Time.deltaTime;
-        }
-        x = x;
-        float y_=y;
-        if(y<GROUND_Y){
-            y_ = GROUND_Y;
-        }
-        z = z;
+            sphereState.vel_y += gravity*Time.deltaTime;
+            sphereState.pos_y -= sphereState.vel_y*Time.deltaTime;
 
-        transform.position = new Vector3(x, y_, z);
+            if(sphereIsActive && sphereState.pos_y <= GROUND_Y + 0.015f){ //カメラが球体の中に侵入しないようにするために非アクティブにする距離
+                sphereIsActive = false;
+            }
+            if(!sphereIsActive && sphereState.pos_y >= GROUND_Y + 0.015f){
+                sphereIsActive = true;
+            }
+            sphere.SetActive(sphereIsActive);
+
+            // cylinderのサイズ調整
+            Vector3 scale = cylinder.transform.localScale;
+            float radius = -sphereState.pos_y * radius_coef;
+            if(radius < 0.0f){
+                radius = 0.0f;
+            }
+            scale.x = radius;
+            scale.y = 0.0f;
+            scale.z = radius;
+            cylinder.transform.localScale = scale;
+
+        }
+                    
+
+        sphere.transform.position = new Vector3(sphereState.pos_x, sphereState.pos_y, sphereState.pos_z);
+        cylinder.transform.position = new Vector3(cylinderState.pos_x, cylinderState.pos_y, cylinderState.pos_z);
     }
 }
